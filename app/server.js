b28f3e3a2f93b531a26a3cc3e041f72c851e51b4
Maika678 <1776430@cegepmontpetit.ca>
2026-04-10 16:35:00 -0400

    Change message for qa

--- app/server.js
+++ app/server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de l'environnement de DEV",
-  qa: "Hello de l'environnement de QA",
+  dev: "Bonjour de l'environnement de DEV",
+  qa: "Bonjour de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
 
